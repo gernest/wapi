7e845298a63962ff848a9831d9c057ac080f58b2
gernest <geofreyernest@live.com>
2018-12-19 18:52:33 +0300

    add stuff

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,31 @@
 const std = @import("std");
+const ast = std.zig.ast;
 
 pub fn main() anyerror!void {
     std.debug.warn("All your base are belong to us.\n");
 }
+
+const Package = struct {
+    doc: ?[]const u8,
+};
+
+const ObjectList = std.ArrayList(*Object);
+
+// Object describes a documented object. We only document public API which is
+// evaluated from top down. This means a if a parent is private, all it will be
+// ignored together with its members even if some/all of its members are public..
+//
+const Object = struct {
+    path: []const u8,
+    doc: ?[]const u8,
+    args: ?ObjectList,
+    location: ast.Location,
+
+    const Encoding = struct {
+        const Enum = 'E';
+        const Field = 'E';
+        const Func = 'C';
+        const Method = 'M';
+        const Struct = 'S';
+    };
+};
