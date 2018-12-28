d639c3d3a2ee66fc83c9c3ee7f28f791a97f4c33
gernest <geofreyernest@live.com>
2018-12-28 11:06:12 +0300

    setup commandline

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,81 @@
 const std = @import("std");
-const ast = std.zig.ast;
-pub fn main() anyerror!void {
-    std.debug.warn("All your base are belong to us.\n");
+const clap = @import("clap");
+const mem = std.mem;
+const io = std.io;
+const json = std.json;
+const path = std.os.path;
+const Sha3_256 = std.crypto.Sha3_256;
+const warn = std.debug.warn;
+const Dir = std.os.Dir;
+const Entry = std.os.Dir.Entry;
+const base64 = std.base64.standard_encoder;
+
+fn generateDocs(allocator: *std.mem.Allocator, full_path: []const u8) !void {
+    var buf = &try std.Buffer.init(allocator, "");
+    defer buf.deinit();
+    var stream = io.BufferOutStream.init(buf);
+    try walkTree(allocator, &stream.stream, full_path);
+    warn("{}\n", buf.toSlice());
 }
 
-test "all" {
-    _ = @import("parse.zig");
+fn walkTree(allocator: *std.mem.Allocator, stream: var, full_path: []const u8) anyerror!void {
+    var dir = try Dir.open(allocator, full_path);
+    defer dir.close();
+    var full_entry_buf = std.ArrayList(u8).init(allocator);
+    defer full_entry_buf.deinit();
+    var h = Sha3_256.init();
+    var out: [Sha3_256.digest_length]u8 = undefined;
+
+    while (try dir.next()) |entry| {
+        if (entry.name[0] == '.' or mem.eql(u8, entry.name, "zig-cache")) {
+            continue;
+        }
+        try full_entry_buf.resize(full_path.len + entry.name.len + 1);
+        const full_entry_path = full_entry_buf.toSlice();
+        mem.copy(u8, full_entry_path, full_path);
+        full_entry_path[full_path.len] = path.sep;
+        mem.copy(u8, full_entry_path[full_path.len + 1 ..], entry.name);
+        switch (entry.kind) {
+            Entry.Kind.File => {
+                const content = try io.readFileAlloc(allocator, full_entry_path);
+                errdefer allocator.free(content);
+                try stream.print("{s}\n", full_entry_path);
+                allocator.free(content);
+            },
+            Entry.Kind.Directory => {
+                try walkTree(allocator, stream, full_entry_path);
+            },
+            else => {},
+        }
+    }
+}
+
+pub fn main() !void {
+    const stdout_file = try std.io.getStdOut();
+    var stdout_out_stream = stdout_file.outStream();
+    const stdout = &stdout_out_stream.stream;
+
+    var direct_allocator = std.heap.DirectAllocator.init();
+    const allocator = &direct_allocator.allocator;
+    defer direct_allocator.deinit();
+
+    // First we specify what parameters our program can take.
+    const params = comptime []clap.Param([]const u8){clap.Param([]const u8).positional("path")};
+
+    var os_iter = clap.args.OsIterator.init(allocator);
+    const iter = &os_iter.iter;
+    defer os_iter.deinit();
+
+    var buf = &try std.Buffer.init(allocator, "");
+    defer buf.deinit();
+
+    const exe = try iter.next();
+    var args = try clap.ComptimeClap([]const u8, params).parse(allocator, clap.args.OsIterator.Error, iter);
+    defer args.deinit();
+    const pos = args.positionals();
+    if (pos.len != 1) {
+        warn("missing  path");
+        return try clap.help(stdout, params);
+    }
+    try generateDocs(allocator, pos[0]);
 }
