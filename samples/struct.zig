5c39e7291a2d848e456bbb0fe0d9512576167715
gernest <geofreyernest@live.com>
2018-12-30 22:26:21 +0300

    unused

--- samples/struct.zig
+++
@@ -1,5 +0,0 @@
-/// TestStruct this is ideal test documenting a struct object.
-pub const TestStruct = struct {
-    /// methodWithNoArs takes no argument and returns nothing.
-    pub fn methodWithNoArs() void {}
-};
